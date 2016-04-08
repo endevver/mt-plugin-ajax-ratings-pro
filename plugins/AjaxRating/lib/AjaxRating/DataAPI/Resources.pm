bef862fffb1d784cec1bba40aeb4670d5f2b21d0
Dan Wolfgang <dan@uinnovations.com>
2016-04-08 15:31:00 -0400

    Data API: added a "ratings" hash with summary data to Entries. Bump to version 1.5.1.

---
+++ plugins/AjaxRating/lib/AjaxRating/DataAPI/Resources.pm
@@ -0,0 +1,58 @@
+package AjaxRating::DataAPI::Resources {
+
+    use strict;
+    use warnings;
+
+    # Add a `ratings` hash to an entry object in the data API.
+    sub entry_summary {
+        return [
+            {
+                name => 'ratings',
+                from_object => sub {
+                    my ($entry) = @_;
+                    my $vote_summ
+                        = MT->instance->model('ajaxrating_votesummary')->load({
+                            obj_type => 'entry',
+                            obj_id   => $entry->id,
+                        }) or return {
+                            avg_score   => 0,
+                            total_score => 0,
+                            vote_count  => 0,
+                        };
+
+                    return {
+                        avg_score   => $vote_summ->avg_score,
+                        total_score => $vote_summ->total_score,
+                        vote_count  => $vote_summ->vote_count,
+                    };
+                },
+                bulk_from_object => sub {
+                    my ( $entries, $hashes ) = @_;
+
+                    for my $i ( 0 .. ( scalar(@$entries) - 1 ) ) {
+                        my $entry = $entries->[$i];
+                        my $vote_summ
+                            = MT->instance->model('ajaxrating_votesummary')->load({
+                                obj_type => 'entry',
+                                obj_id   => $entry->id,
+                            }) or return $hashes->[$i]->{"ratings"} = {
+                                avg_score   => 0,
+                                total_score => 0,
+                                vote_count  => 0,
+                            };
+
+                        $hashes->[$i]->{"ratings"} = {
+                            avg_score   => $vote_summ->avg_score,
+                            total_score => $vote_summ->total_score,
+                            vote_count  => $vote_summ->vote_count,
+                        };
+                    }
+                },
+            }
+        ];
+    }
+}
+
+1;
+
+__END__
