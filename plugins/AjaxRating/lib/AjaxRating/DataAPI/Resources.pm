ab129f047cc0b523aa41afde3c70abe8c7424b97
Jay Allen <jay@endevver.com>
2016-04-22 15:59:11 -0700

    Version v1.5.3: DataAPI fixes/improvements
    * Fixed a bug where In some cases, the ratings hash would not appear in the Data API responses
    * Ensured that all ratings were output as numbers instead of strings
    * Changed output fields to be consistent with MT's Data API format (javascriptStyle)
    * Added userRating into the hash when the current user has voted on the current item in context
    * SIGNIFICANTLY slimmed down all of that crazy code

--- plugins/AjaxRating/lib/AjaxRating/DataAPI/Resources.pm
+++ plugins/AjaxRating/lib/AjaxRating/DataAPI/Resources.pm
@@ -6,55 +6,43 @@ package AjaxRating::DataAPI::Resources {
     # Add a `ratings` hash to an entry object in the data API.
     sub entry_summary {
         return [
-            {
-                name => 'ratings',
-                from_object => sub {
-                    my ($entry) = @_;
-                    my $vote_summ
-                        = MT->instance->model('ajaxrating_votesummary')->load({
-                            obj_type => 'entry',
-                            obj_id   => $entry->id,
-                        }) or return {
-                            avg_score   => 0,
-                            total_score => 0,
-                            vote_count  => 0,
-                        };
-
-                    return {
-                        avg_score   => $vote_summ->avg_score,
-                        total_score => $vote_summ->total_score,
-                        vote_count  => $vote_summ->vote_count,
-                    };
-                },
+            {   name             => 'ratings',
+                from_object      => \&from_object,
                 bulk_from_object => sub {
                     my ( $entries, $hashes ) = @_;
-
-                    for my $i ( 0 .. ( scalar(@$entries) - 1 ) ) {
-                        my $entry = $entries->[$i];
-                        my $vote_summ
-                            = MT->instance->model('ajaxrating_votesummary')->load({
-                                obj_type => 'entry',
-                                obj_id   => $entry->id,
-                            });
-
-                        if ($vote_summ) {
-                            $hashes->[$i]->{"ratings"} = {
-                                avg_score   => $vote_summ->avg_score,
-                                total_score => $vote_summ->total_score,
-                                vote_count  => $vote_summ->vote_count,
-                            };
-                        }
-                        else {
-                            $hashes->[$i]->{"ratings"} = {
-                                avg_score   => 0,
-                                total_score => 0,
-                                vote_count  => 0,
-                            };
-                        }
-                    }
+                    my $i = 0;
+                    $hashes->[$i++]->{ratings} = from_object( $_ )
+                        foreach @$entries;
                 },
             }
         ];
+    }
+
+    sub from_object {
+        my ( $entry ) = @_;
+        my $app       = MT->instance;
+        my $Summary   = $app->model('ajaxrating_votesummary');
+        my $Vote      = $app->model('ajaxrating_vote');
+        my %terms     = ( obj_type => 'entry', obj_id => $entry->id );
+        my $data      = {};
+
+        # Add current user's rating, if one exists
+        my $user = $app->user;
+        if ( $user && $user->id ) { # Saved and not anonymous
+            if ( my $vote = $Vote->load({ voter_id => $user->id, %terms }) ) {
+                $data->{userRating} = 0 + $vote->score;
+            }
+        }
+
+        my %map = ( # MT uses camel-case for the Data API
+            avg_score   => 'averageScore',
+            total_score => 'totalScore',
+            vote_count  => 'voteCount',
+        );
+        my $summary       = $Summary->get_by_key( \%terms );
+        $data->{$map{$_}} = 0 + ($summary->$_||0) foreach keys %map;
+
+        return $data;
     }
 }
 
